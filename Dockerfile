7c5d4299bfc77de6383e07b1f470c79e41bfe46e
Kyp <kyp108@gmail.com>
2018-06-09 23:59:17 +0200

    init/migrate db at start of main container

--- Dockerfile
+++ Dockerfile
@@ -74,5 +74,8 @@ ENV RAILS_ENV=production \
     RAILS_LOG_TO_STDOUT=true \
     RAILS_SERVE_STATIC_FILES=true
 
-# run the server
-CMD [ "bin/rails s -p 3000 -b 0.0.0.0" ]
+# copy entrypoint
+COPY docker-entrypoint.sh /usr/local/bin
+
+# Run puma server
+ENTRYPOINT ["docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,19 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-exec "$@"
+echo "Waiting for Postgres to start..."
+if [[ "${RAILS_ENV}" = "production" ]]; then
+  while ! nc -z production-postgres 5432; do sleep 0.1; done
+else
+  while ! nc -z db 5432; do sleep 0.1; done
+fi
+echo "Postgres is up"
+
+if [ "$1" = "server" ]; then
+  echo "preparing database..."
+  rails db:exists && rails db:migrate || rails db:setup
+  echo "database ready, starting server"
+  bundle exec rails s -p 3000 -b 0.0.0.0
+else
+  exec "$@"
+fi
